58b47f75169ac7804a69bffc5d0f6495f5d6eacc
amanp <amanp@DESKTOP-UJ95DB3>
2025-09-15 12:31:39 +0530

    TEst

--- EmployeeDataAccess/Properties/AssemblyInfo.cs
+++ EmployeeDataAccess/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EmployeeDataAccess")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("")] 
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EmployeeDataAccess")]
