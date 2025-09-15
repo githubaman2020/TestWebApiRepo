d76b5fde14ff025fc04dd3ce726eb20097aa65de
amanp <amanp@DESKTOP-UJ95DB3>
2025-09-15 12:36:02 +0530

    test2

---
+++ WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.ModelBinding;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+namespace WebApi
+{
+    public static class WebApiConfig
+    {
+        //public class CustomJsonFormtter : JsonMediaTypeFormatter {
+
+        //    public CustomJsonFormtter() 
+        //    {
+        //        this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html")); 
+        //    }
+
+        //    public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        //    {
+        //        base.SetDefaultContentHeaders(type, headers, mediaType);
+        //        headers.ContentType=new MediaTypeHeaderValue("application/json")
+        //    }
+        //}
+
+        public static void Register(HttpConfiguration config)
+        {
+
+            //config.SuppressDefaultHostAuthentication();
+            //config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+           
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //config.Formatters.Add(new CustomJsonFormtter());
+            //config.Formatters.JsonFormatter.SerializerSettings.Formatting=Newtonsoft.Json.Formatting.Indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+           // config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);// Retrun only Json Data
+            //config.Formatters.Remove(config.Formatters.JsonFormatter);// Retrun only XML Data
+        }
+    }
+}
---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+
+        public static List<string> strings = new List<string>()
+        {
+        "Value1","Value2","Value3"
+        };
+        public IEnumerable<string> Get()
+        {
+            return strings;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return strings[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+            strings.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+            strings[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            strings.RemoveAt(id);  
+        }
+    }
+}
